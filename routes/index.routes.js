7355816219154a2861dfaa1e84ea6d4132230672
Antonio Gloria <angloria87@gmail.com>
2023-09-21 10:20:20 -0600

    removed unnecessary preview route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,7 +4,6 @@ const multer = require("multer")
 const upload = multer({ dest: 'print-files' })
 const ptp = require("pdf-to-printer")
 const fs = require("fs")
-const path = require("path")
 
 router.get("/", (req, res, next) => {
   res.json("All good in here")
@@ -18,23 +17,6 @@ router.post("/upload", upload.single("file"), async (req, res, next) => {
   catch (error) {
     next(error)
   }
-})
-
-router.get("/preview", async (req, res, next) => {
-  try {
-    /* const { file } = req.params
-    console.log(file) */
-    //console.log(__dirname);
-    //console.log(path.dirname("../print-files/ca71c3868e5f2fd47c119dc27429eadc"));
-    const abs = path.resolve("print-files\\37dae1eec3520be4d43ac8476deec190")
-    console.log(abs);
-    res.sendFile(abs)
-
-  }
-  catch (error) {
-    next(error)
-  }
-
 })
 
 router.post("/print", async (req, res, next) => {
